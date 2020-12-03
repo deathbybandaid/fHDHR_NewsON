7473f489e087bd8ce11edc54a25fdd1101ae066b
deathbybandaid <github@deathbybandaid.net>
2020-12-03 17:28:35 -0500

    test

--- fHDHR/http/hdhr/device_xml.py
+++ fHDHR/http/hdhr/device_xml.py
@@ -23,7 +23,7 @@ class HDHR_Device_XML():
         out = xml.etree.ElementTree.Element('root')
         out.set('xmlns', "urn:schemas-upnp-org:device-1-0")
 
-        sub_el(out, 'URLBase', "%s/hdhr/" % base_url)
+        sub_el(out, 'URLBase', "%s" % base_url)
 
         specVersion_out = sub_el(out, 'specVersion')
         sub_el(specVersion_out, 'major', "1")
