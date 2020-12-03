2667d8c6690b1c2e9f84bea35cb5f0a96f378dd1
deathbybandaid <github@deathbybandaid.net>
2020-12-03 08:01:42 -0500

    test

--- fHDHR/http/files/device_xml.py
+++ fHDHR/http/files/device_xml.py
@@ -19,22 +19,44 @@ class Device_XML():
 
         base_url = request.url_root[:-1]
 
+        user_agent = request.headers.get('User-Agent')
+
         out = xml.etree.ElementTree.Element('root')
         out.set('xmlns', "urn:schemas-upnp-org:device-1-0")
 
-        sub_el(out, 'URLBase', base_url)
+        if not (self.fhdhr.config.dict["rmg"]["enabled"] and str(user_agent).lower().startswith("plexmediaserver")):
+            sub_el(out, 'URLBase', base_url)
 
         specVersion_out = sub_el(out, 'specVersion')
         sub_el(specVersion_out, 'major', "1")
         sub_el(specVersion_out, 'minor', "0")
 
         device_out = sub_el(out, 'device')
-        sub_el(device_out, 'deviceType', "urn:schemas-upnp-org:device:MediaServer:1")
+
+        if self.fhdhr.config.dict["rmg"]["enabled"] and str(user_agent).lower().startswith("plexmediaserver"):
+            sub_el(device_out, 'deviceType', "urn:plex-tv:device:Media:1")
+        else:
+            sub_el(device_out, 'deviceType', "urn:schemas-upnp-org:device:MediaServer:1")
+
         sub_el(device_out, 'friendlyName', self.fhdhr.config.dict["fhdhr"]["friendlyname"])
         sub_el(device_out, 'manufacturer', self.fhdhr.config.dict["fhdhr"]["reporting_manufacturer"])
+        sub_el(device_out, 'manufacturerURL', "https://github.com/fHDHR/" % self.fhdhr.config.dict["main"]["reponame"])
         sub_el(device_out, 'modelName', self.fhdhr.config.dict["fhdhr"]["reporting_model"])
-        sub_el(device_out, 'modelNumber', self.fhdhr.config.dict["fhdhr"]["reporting_model"])
-        sub_el(device_out, 'serialNumber')
+        sub_el(device_out, 'modelNumber', self.fhdhr.config.internal["versions"]["fHDHR"])
+
+        if self.fhdhr.config.dict["rmg"]["enabled"] and str(user_agent).lower().startswith("plexmediaserver"):
+            sub_el(device_out, 'modelDescription', self.fhdhr.config.dict["fhdhr"]["friendlyname"])
+            sub_el(device_out, 'modelURL', "https://github.com/fHDHR/" % self.fhdhr.config.dict["main"]["reponame"])
+        else:
+            sub_el(device_out, 'serialNumber')
+
+        if self.fhdhr.config.dict["rmg"]["enabled"] and str(user_agent).lower().startswith("plexmediaserver"):
+            serviceList_out = sub_el(device_out, 'serviceList')
+            service_out = sub_el(serviceList_out, 'service')
+            sub_el(service_out, 'URLBase', base_url)
+            sub_el(service_out, 'serviceType', "urn:plex-tv:service:MediaGrabber:1")
+            sub_el(service_out, 'serviceId', "urn:plex-tv:serviceId:MediaGrabber")
+
         sub_el(device_out, 'UDN', "uuid:" + self.fhdhr.config.dict["main"]["uuid"])
 
         fakefile = BytesIO()
