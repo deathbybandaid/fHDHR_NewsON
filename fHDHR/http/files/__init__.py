d9e166884eecf468f97a555fccea580b6f502113
deathbybandaid <github@deathbybandaid.net>
2020-12-03 17:24:26 -0500

    test

--- fHDHR/http/files/__init__.py
+++ fHDHR/http/files/__init__.py
@@ -2,6 +2,7 @@
 
 from .favicon_ico import Favicon_ICO
 from .style_css import Style_CSS
+from .device_xml import Device_XML
 
 
 class fHDHR_Files():
@@ -11,3 +12,4 @@ class fHDHR_Files():
 
         self.favicon = Favicon_ICO(fhdhr)
         self.style = Style_CSS(fhdhr)
+        self.device_xml = Device_XML(fhdhr)
